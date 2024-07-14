cce5ac36bdec19299b5e7c05d57316d2d5ff6e84
Lucas Vido <956388+Vido@users.noreply.github.com>
2024-07-14 16:21:31 -0300

    Update tasks.py

--- static_sitemaps/tasks.py
+++ static_sitemaps/tasks.py
@@ -6,8 +6,27 @@ except:
     # fallback
     Task = object
     
+try:
+    from huey.contrib.djhuey import periodic_task
+except:
+    # https://github.com/coleifer/huey/blob/0b6722a64f408d7d4b37a8eb8b38fa24aa61f5de/huey/contrib/mini.py#L69C1-L72C25
+    def periodic_task(self, validate_func):
+        def decorator(fn):
+            return self.task(validate_func)(fn)
+        return decorator
+
 from static_sitemaps import conf
 from static_sitemaps.generator import SitemapGenerator
+
+def loop_x_minutes(minutes=60):
+    def validate_date(timestamp):
+        return timestamp.second % seconds == 0
+    return validate_date
+    
+@periodic_task(loop_x_minutes())
+def generate_sitemap_task(number):
+    generator = SitemapGenerator(verbosity=1)
+    generator.write()
 
 __author__ = 'xaralis'
 
